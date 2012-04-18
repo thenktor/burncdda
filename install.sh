b926caed696bf31677c28bc435400aec9ff6070d
thenktor <thenktor@b87fc89c-4183-45fe-872d-c3e2f47bb33b>
2012-04-18 09:37:26 +0000

    contact info updated

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 # burnCDDA is a shell script for burning Audio-CDs with cdrdao
 # and cdrecord.
 #
-# (C) 2001 by Thorsten Muehlfelder <thenktor@gmx.de>
+# (C) 2001 by Thorsten Muehlfelder <~thenktor~at~gmail~dot~com~>
 # burnCDDA comes with ABSOLUTELY NO WARRANTY.
 # This is free software, and you are welcome to redistribute it
 # under terms of the GNU General Public License (version 2)
